cb0afcd7be8f6f3b1edc5d4fa36724b21400aabd
RodrigoCBArj <rodrigocbarj@gmail.com>
2022-12-23 13:05:34 -0300

    configurando envio de mensagem

--- ecommerce/src/main/java/br/com/rodrigocbarj/Main.java
+++
@@ -1,7 +0,0 @@
-package br.com.rodrigocbarj;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Vamos estudar Kafka!");
-    }
-}
---
+++ ecommerce/src/main/java/br/com/rodrigocbarj/NewOrderMain.java
@@ -0,0 +1,34 @@
+package br.com.rodrigocbarj;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+public class NewOrderMain {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        var producer = new KafkaProducer<String, String>(properties());
+        var value = "172378912, 3245, 89.90";
+        var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", value, value);
+        producer.send(record, (data, exception) -> {
+            if (exception != null) {
+                exception.printStackTrace();
+                return;
+            }
+            System.out.println("MENSAGEM ENVIADA COM SUCESSO PARA: " + data.topic() +
+                    "\npartição: " + data.partition() +
+                    "\noffset: " + data.offset() +
+                    "\ntimestamp: " + data.timestamp());
+        }).get();
+    }
+
+    private static Properties properties() {
+        var properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        return properties;
+    }
+}
