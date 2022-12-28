4463e32bde907757532abcf995955c85c3ea600f
RodrigoCBArj <rodrigocbarj@gmail.com>
2022-12-28 14:19:49 -0300

    serviço de log - "escuta" mais de um canal

---
+++ ecommerce/src/main/java/br/com/rodrigocbarj/LogService.java
@@ -0,0 +1,43 @@
+package br.com.rodrigocbarj;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+import java.util.regex.Pattern;
+
+public class LogService {
+
+    public static void main(String[] args) {
+        var consumer = new KafkaConsumer<String, String>(properties());
+        consumer.subscribe(Pattern.compile("ECOMMERCE.*"));
+
+        while (true) {
+            var records = consumer.poll(Duration.ofMillis(2000));
+
+            if (!records.isEmpty()) {
+                for (var record : records) {
+                    // "envio de email"
+                    System.out.println("##### LOG DA MENSAGEM: #####");
+                    System.out.println(record.topic() +
+                            "\nkey: " + record.key() +
+                            "\nvalue: " + record.value() +
+                            "\nparticao: " +record.partition() +
+                            "\noffset: " + record.offset());
+                }
+            }
+        }
+    }
+
+    private static Properties properties() {
+        var properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, LogService.class.getSimpleName());
+        return properties;
+    }
+}
